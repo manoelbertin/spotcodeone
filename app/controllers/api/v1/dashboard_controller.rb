bfad5e54014b7e4ea82b14cf987d5445e10b8a58
manoel bertin <manoeltirj@gmail.com>
2023-02-02 21:08:21 -0300

    tela onde apresento os últimos álbuns escutados e as recomendações de músicas e categorias ordenadas pelo número de vezes tocadas.

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -3,6 +3,7 @@ module Api::V1
   
     def index
       load_recent_heard
+      loal_recommendations
     end
 
     private
@@ -11,8 +12,14 @@ module Api::V1
       @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album)
     end
 
-    def 
-      
+    def loal_recommendations
+      heard_categories = @recent_albums.map(&:category)
+      if heard_categories.present?
+        @recommend_albums = Album.joins(:category, :songs).where(category: heard_categories).order('songs.played_count')
+                                                          .select('distinct albums.*').limit(12)
+      else
+        @recommend_albums = Album.all.sample(12)
+      end
     end
   end
 end
