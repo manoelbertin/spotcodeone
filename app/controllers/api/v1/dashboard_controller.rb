ee2a42fdfa4d4a137445c1f07cfcee3867e4b655
manoel bertin <manoeltirj@gmail.com>
2023-02-04 18:20:15 -0300

    tive que comentar parte no controller dashboard onde ondena por songs, porque estava dando erro no Postgre.

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -1,25 +1,52 @@
-module Api::V1
-  class DashboardController < ApplicationController
-  
-    def index
-      load_recent_heard
-      load_recommendations
-    end
 
-    private
-
-    def load_recent_heard
-      @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album)
-    end
-
-    def load_recommendations
-      heard_categories = @recent_albums.map(&:category)
-      if heard_categories.present?
-        @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories).order('songs.played_count')
-                                                          .select('distinct albums.*').limit(12)
-      else
-        @recommended_albums = Album.all.sample(12)
-      end
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+class Api::V1::DashboardController < ApplicationController
+  def index
+    load_recent_heard
+    load_recommendations
+  end
+ 
+ 
+  private
+ 
+ 
+  def load_recent_heard
+    @recent_albums = current_user.recently_heards.order("created_at DESC").limit(4).map(&:album)
+  end
+ 
+ 
+  def load_recommendations
+    heard_categories = @recent_albums.map(&:category)
+    if heard_categories.present?
+      @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories)
+                                 #.order('songs.played_count')
+                                 .select('distinct albums.*').limit(12)
+    else
+      @recommended_albums = Album.all.limit(12)
     end
   end
 end
--- app/views/api/v1/dashboard/index.json.jbuilder
+++ app/views/api/v1/dashboard/index.json.jbuilder
@@ -4,7 +4,7 @@ json.recent_albums @recent_albums.each do |album|
   json.artist_name album.artist.name
   json.cover_url url_for(album.cover)
 end
-
+ 
 json.recommended_albums @recommended_albums.each do |album|
   json.id album.id
   json.title album.title
