2149dce1acf1007624319a2608e9136d82a2b578
manoel bertin <manoeltirj@gmail.com>
2023-02-04 21:23:06 -0300

    buscando dentro dos artistas onde é que a gente tem álbuns com esses ids. vamos ver qual id é igual a da categoria de que a gente está buscando.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,11 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   has_one_attached :image
+
+  def artists
+    Artist.join(:albums).where(albums: { id: self.albums.ids })
+  end
+
+  def songs
+  end
 end
