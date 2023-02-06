23d8525a64ea9779d0c0a82919073f59ae5a5473
manoel bertin <manoeltirj@gmail.com>
2023-02-06 09:01:10 -0300

    add um merge para passar parâmetros e add um concerns em songs pra reaproveitamento de rotas por exemplo POST  pra  /api/v1/songs/:id/favorite, e DELETE pra /api/v1/songs/:id/favorite.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
+
+  concern :favoritable do |options|
+    shallow do 
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite', { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  # /api/v1/songs/favorite
+  # /api/v1/songs/:id/favorite  << -- com o uso do "on: :member" eu incluo o :id
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
@@ -12,6 +23,9 @@ Rails.application.routes.draw do
         resources :recently_heards, only: :create
       end
       resources :favorites, only: :index
+
+      resources :songs, only: [] do 
+        concerns :favoritable, favoritable_type: 'Song'
     end
   end
 end
