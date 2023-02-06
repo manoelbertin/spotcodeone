e96be7732d03219c2ab7c8accab6fbd5ae4bcf89
manoel bertin <manoeltirj@gmail.com>
2023-02-06 08:41:34 -0300

    eu incrementei as actions CREATE e DESTROY no meu controller favorites. Sendo que meu response não era um jbuilder mas sim o código 200. ok

--- app/controllers/api/v1/recently_heards_controller.rb
+++ app/controllers/api/v1/recently_heards_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecentlyHeardsController < ApplicationController
   def create
     @recently_heards = current_user.recently_heards.create(album_id: params[:album_id])
+    @recently_heard.save
     head :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       resources :albums, only: :show do 
         resources :recently_heards, only: :create
       end
+      resources :favorites, only: :index
     end
   end
 end
